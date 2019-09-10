9e616c0b3208f3440e3e9e69e6baede5da78fc13
Alvee Akand <AlveeM@users.noreply.github.com>
2019-09-10 16:25:44 +0600

    added heroku database

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://tranquil-falls-04302.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -105,7 +105,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://tranquil-falls-04302.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
